4ab76943b589b5d3beae4537a737b58ce03c42cf
ZenithNyoka <zensarutobi@gmail.com>
2026-02-05 15:30:49 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,34 +1,32 @@
-const CACHE_NAME = "quiz-cache-v2";
+const CACHE_NAME = "dsquiz-v3";
 
 const ASSETS = [
   "./",
   "./index.html",
   "./style.css",
   "./script.js",
-  "./tree.png",
-  "./manifest.json"
+  "./manifest.json",
+  "./tree.png"
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))
   );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
+self.addEventListener("activate", e => {
+  e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-      )
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
     )
   );
   self.clients.claim();
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
